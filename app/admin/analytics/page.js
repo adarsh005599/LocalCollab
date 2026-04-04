241087b46a34e797524efdd75b003999c1ab692a
Adarsh Singh <happysingh005599@gmail.com>
2026-04-04 08:13:30 +0530

    Update page.js

--- app/admin/analytics/page.js
+++ app/admin/analytics/page.js
@@ -1,5 +1,5 @@
 "use client";
-import Topbar from "@/components/admin/Topbar";
+import Topbar from "../../../components/admin/Topbar";
 import { WEEKLY_MATCHES, STATS } from "@/lib/adminData";
 
 const MONTHLY = [
